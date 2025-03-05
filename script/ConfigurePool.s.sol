f36b7efa63a1f8909c0bc99e4c4745c408f8ea73
ybtuti <samuelmsuu@gmail.com>
2025-03-05 20:50:34 +0300

    Add scripts for token bridging, pool configuration, and deployment

---
+++ script/ConfigurePool.s.sol
@@ -0,0 +1,43 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.24;
+
+import {Script} from "forge-std/Script.sol";
+import {TokenPool} from "@ccip/contracts/src/v0.8/ccip/pools/TokenPool.sol";
+import {RateLimiter} from "@ccip/contracts/src/v0.8/ccip/libraries/RateLimiter.sol";
+
+contract ConfigurePoolScript is Script {
+    function run(
+        address localPool,
+        uint64 remoteChainSelector,
+        address remotePool,
+        address remoteToken,
+        bool outboundRateLimiterIsEnabled,
+        uint128 outboundRateLimiterCapacity,
+        uint128 outboundRateLimiterRate,
+        bool inboundRateLimiterIsEnabled,
+        uint128 inboundRateLimiterCapacity,
+        uint128 inboundRateLimiterRate
+    ) public {
+        vm.startBroadcast();
+        // @ybtuti Make contributions to the cyfrin repo on this
+        TokenPool.ChainUpdate[] memory chainsToAdd = new TokenPool.ChainUpdate[](1);
+        chainsToAdd[0] = TokenPool.ChainUpdate({
+            remoteChainSelector: remoteChainSelector,
+            remotePoolAddress: abi.encode(remotePool),
+            remoteTokenAddress: abi.encode(remoteToken),
+            outboundRateLimiterConfig: RateLimiter.Config({
+                isEnabled: outboundRateLimiterIsEnabled,
+                capacity: outboundRateLimiterCapacity,
+                rate: outboundRateLimiterRate
+            }),
+            inboundRateLimiterConfig: RateLimiter.Config({
+                isEnabled: inboundRateLimiterIsEnabled,
+                capacity: inboundRateLimiterCapacity,
+                rate: inboundRateLimiterRate
+            }),
+            allowed: true
+        });
+        TokenPool(localPool).applyChainUpdates(chainsToAdd);
+    }
+}
--- src/interfaces/IRebaseToken.sol
+++ src/interfaces/IRebaseToken.sol
@@ -8,4 +8,5 @@ interface IRebaseToken {
     function balanceOf(address _user) external view returns (uint256);
     function getUserInterestRate(address _user) external view returns (uint256);
     function getInterestRate() external view returns (uint256);
+    function grantMintAndBurnRole(address _account) external;
 }
